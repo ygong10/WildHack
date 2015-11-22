8d0fdef254e5f32bb9a5d6dc09b988435a044f91
ygong10 <yichenggong567@gmail.com>
2015-11-21 19:40:31 -0600

    yo

--- app.js
+++ app.js
@@ -4,17 +4,30 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+function getRandomColor() {
+ 	var letters = '0123456789ABCDEF'.split('');
+ 	var color = '#';
+	for (var i = 0; i < 6; i++ ) {
+    	color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
 app.get('/', function(req, res){
 	 res.sendFile(__dirname + '/index.html');
 });
 var id = 1;
+var random_color = getRandomColor();
 io.on('connection', function(socket){
 	var my_id = id ;
-	io.emit('user connected', 'Anon#' + my_id + ' is connected')
+	var rand_color = random_color;
+	io.emit('user connected', ('Anon#' + my_id).fontcolor(rand_color) + ' is connected')
 	socket.on('chat message', function(msg){
-		io.emit('chat message', my_id + ": " + msg);
+		var test = "Anon#"+ my_id;
+		var test_it = test.fontcolor(rand_color);
+		io.emit('chat message', test_it + ": " + msg);
 	});
 	id++;
+	random_color = getRandomColor();
 });
 
 	http.listen(port, function(){
