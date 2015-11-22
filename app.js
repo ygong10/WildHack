f95813ddb146c4334aa94ec125774b74de3680d2
ygong10 <yichenggong567@gmail.com>
2015-11-21 18:23:50 -0600

    'yo

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
 	 res.sendFile(__dirname + '/index.html');
 });
-	id = 0;
-	io.on('connection', function(socket){
-		socket.on('chat message', function(msg){
-		io.emit('chat message', msg+id);
- 	});
-});
+var id = 1;
+io.on('connection', function(socket){
+	var my_id = id ;
+	io.emit('user connected', 'User' + my_id + ' connected')
+	socket.on('chat message', function(msg){
+		io.emit('chat message', my_id + ": " + msg);
+	});
 	id++;
+});
 
 	http.listen(port, function(){
 	 	console.log('listening on ' + port);
